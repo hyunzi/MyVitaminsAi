ef36dc769db2f099d799d00fe129b127218296a8
jyeonSong <private9084@gmail.com>
2024-07-31 00:19:12 +0900

    [BE] ImageService protocol

---
+++ api/src/main/java/com/mva/api/myvitamin/service/ImageService.java
@@ -0,0 +1,5 @@
+package com.mva.api.myvitamin.service;
+
+public interface ImageService {
+    public String getBase64Image(String imgUrl);
+}
---
+++ api/src/main/java/com/mva/api/myvitamin/service/impl/ImageServiceImpl.java
@@ -0,0 +1,41 @@
+package com.mva.api.myvitamin.service.impl;
+
+
+import com.mva.api.myvitamin.service.ImageService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Base64;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class ImageServiceImpl implements ImageService {
+    public String getBase64Image(String imgUrl) {
+        String base64Image = "";
+        try {
+            URL url = new URL(imgUrl);
+
+            BufferedImage image = ImageIO.read(url);
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(image, "jpg", baos);
+            baos.flush();
+            byte[] imageInBytes = baos.toByteArray();
+            baos.close();
+
+            base64Image = Base64.getEncoder().encodeToString(imageInBytes);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return base64Image;
+    }
+}
