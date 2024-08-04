7f59b4957816a0a735b53537fb4e25705f45b0e0
eunniverse <eunisss98@gmail.com>
2024-08-04 15:30:45 +0900

    [BE] firebase 초기화 시 내부 데이터로 조회하도록 수정
    - FirebaseConfig 에서 FirebaseKey.json 로 조회하도록 수정

--- api/src/main/java/com/mva/api/config/FirebaseConfig.java
+++ api/src/main/java/com/mva/api/config/FirebaseConfig.java
@@ -4,21 +4,32 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
+@Slf4j
 public class FirebaseConfig {
 
     @PostConstruct
     public void init() {
         try {
-            FileInputStream serviceAccount = new FileInputStream("C://firebaseKey.json");
+            ClassPathResource classPathResource = new ClassPathResource("config/firebaseKey.json");
+            File firebaseKeyFile = Paths.get(classPathResource.getURI()).toFile();
+
+            FileInputStream serviceAccount = new FileInputStream(firebaseKeyFile);
             FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
             FirebaseApp.initializeApp(options);
+
         } catch (Exception e) {
             e.printStackTrace();
+            log.error("Failed to initialize Firebase, error: " + e.getMessage());
         }
     }
 }
