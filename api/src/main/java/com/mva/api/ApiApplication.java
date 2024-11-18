b35237f171b7c745203ee7bb3db249d29720ea50
jysong <jysong@ncits.co.kr>
2024-11-18 14:59:48 +0900

    [BE] API Key 보안관리

--- api/src/main/java/com/mva/api/ApiApplication.java
+++ api/src/main/java/com/mva/api/ApiApplication.java
@@ -1,11 +1,17 @@
 package com.mva.api;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiApplication {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();  // .env 파일 로드
+        System.setProperty("GEMINI_API_KEY", dotenv.get("GEMINI_API_KEY"));
+        System.setProperty("GOOGLE_IMAGE_API_KEY", dotenv.get("GOOGLE_IMAGE_API_KEY"));
+        System.setProperty("GOOGLE_IMAGE_ENGINE_ID", dotenv.get("GOOGLE_IMAGE_ENGINE_ID"));
+
         SpringApplication.run(ApiApplication.class, args);
     }
     // todo db insert -> 추후 스케줄링으로 전환
--- api/src/main/java/com/mva/api/config/FirebaseConfig.java
+++ api/src/main/java/com/mva/api/config/FirebaseConfig.java
@@ -3,15 +3,16 @@ package com.mva.api.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+
+import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Configuration
 @Slf4j
@@ -20,7 +21,12 @@ public class FirebaseConfig {
     @PostConstruct
     public void init() {
         try {
-            FileInputStream serviceAccount = new FileInputStream("C://firebaseKey.json");
+            Dotenv dotenv = Dotenv.load();
+            String firebaseConfig64 = dotenv.get("FIREBASE_CONFIG_BASE64");
+            byte[] decodesBytes = Base64.getDecoder().decode(firebaseConfig64);
+            String firebaseConfigJson = new String(decodesBytes);
+
+            InputStream serviceAccount = new ByteArrayInputStream(firebaseConfigJson.getBytes(StandardCharsets.UTF_8));
             FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
             FirebaseApp.initializeApp(options);
 
